df4c2a9552780e7c7b9988c8eee1f6e573325dba
zfrz89 <97602202+zfrz89@users.noreply.github.com>
2023-08-06 11:51:08 +0330

    CartInfrastructure

---
+++ Valnet.Cart.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Valnet.Cart.Application.Contracts.Infrastructure;
+
+namespace Valnet.Cart.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services)
+        {
+            services.AddTransient<IProductService, ProductService>();
+            return services;
+        }
+    }
+}
---
+++ Valnet.Cart.Infrastructure/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using Valnet.Cart.Application.Contracts.Infrastructure;
+
+namespace Valnet.Cart.Infrastructure
+{
+    public class ProductService : IProductService
+    {
+        public async Task<decimal> GetProductPriceAsync(string productId)
+        {
+            if (productId == "1")
+                return (decimal)10.0;
+            if (productId == "2")
+                return (decimal)20.0;            
+            return (decimal)30.0;
+        }
+    }
+}
